64ca26cabeb1636ecbd8334b0034cd845c5e9353
Martin Robles <robles.lprb@gmail.com>
2026-01-10 14:01:46 -0600

    Remove SDK version check in ensureSoftware extension method

--- bgeraser/src/main/java/com/roblescode/bgeraser/utils/BitmapExtensions.kt
+++ bgeraser/src/main/java/com/roblescode/bgeraser/utils/BitmapExtensions.kt
@@ -1,10 +1,9 @@
 package com.roblescode.bgeraser.utils
 
 import android.graphics.Bitmap
-import android.os.Build
 
 fun Bitmap.ensureSoftware(): Bitmap =
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O && config == Bitmap.Config.HARDWARE) {
+    if (config == Bitmap.Config.HARDWARE) {
         copy(Bitmap.Config.ARGB_8888, false)
     } else {
         this
